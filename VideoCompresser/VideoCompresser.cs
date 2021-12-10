04def38bff21707e5c0d402fd0cc9a5fd78344f9
ElrohirGT <elrohirgt@gmail.com>
2021-12-10 11:49:18 -0600

    Updated the console utilities using directive

--- VideoCompresser/VideoCompresser.cs
+++ VideoCompresser/VideoCompresser.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
-using static ConsoleUtilitiesLite.ConsoleUtilitiesLite;
+using static ConsoleUtilitiesLite.ConsoleUtilities;
 
 namespace VideoCompresser
 {
