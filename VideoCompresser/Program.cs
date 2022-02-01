e23fa773c5e9a81418dd20cbcf0fa4566251917e
ElrohirGT <elrohirgt@gmail.com>
2022-02-01 17:51:42 -0600

    FIxed a bug with the way the progress bar is showed

--- VideoCompresser/Program.cs
+++ VideoCompresser/Program.cs
@@ -104,15 +104,17 @@ namespace VideoCompresser
                     }
 
                     StringBuilder builder = new();
-                    float totalPercentage = 0;
                     if (loggingLevel >= (int)LoggingLevel.ShowFolder)
                         builder.AppendLine($"Folder: {report.CurrentDirectory}");
                     if (loggingLevel >= (int)LoggingLevel.ShowProgress)
                         foreach (var item in report.Percentages)
                             builder.AppendLine($"{item.Key}: {item.Value:N2}%");
-                    
+
+                    int denominator = (report.VideosCount == 0) ? 1 : report.VideosCount;
+                    float totalPercentage = report.CompressedVideosCount / denominator * 100;
                     foreach (var percentage in report.Percentages.Values)
-                        totalPercentage += (float)percentage / 100 * 1/report.VideosCount;
+                        totalPercentage += (float)percentage / 100 * 1 / report.VideosCount;
+
                     builder.Append($"Count: {report.CompressedVideosCount}/{report.VideosCount} videos. {ConsoleProgressBar.CreateProgressBar(totalPercentage)}");
 
                     previousLogLength = LogInfoMessage(builder.ToString());
